08a4a634bd186c8cbb5af3435a73b7465be6c82d
Aleksei Piskunov <piskunov.alesha@gmail.com>
2021-03-04 12:42:33 +0700

    📇 Added setup.py and CHANGELOG.md

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="blog_web_api",
+    version="0.1.0",
+    packages=["backend"],
+    url="http://109.248.59.128:8000/docs",
+    author="Aleksei Piskunov",
+    author_email="piskunov.alesha@gmail.com",
+    description="Blog web API"
+)
